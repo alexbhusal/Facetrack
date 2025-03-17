15ca99a115d87a27194db5951e08ddf995d92f4e
Alex Bhusal <145799782+alexbhusal@users.noreply.github.com>
2025-03-17 21:54:09 +0545

    Update ChatBot.jsx

--- components/ChatBot.jsx
+++ components/ChatBot.jsx
@@ -4,8 +4,11 @@ const ChatBot = () => {
   return (
     <div>
       <div id="botpress-webchat"></div>
+{/*   <script src="https://cdn.botpress.cloud/webchat/v2.2/inject.js"></script>
+      <script src="https://files.bpcontent.cloud/2025/03/16/14/20250316140102-PZKRH88E.js"></script> */}
+      
       <script src="https://cdn.botpress.cloud/webchat/v2.2/inject.js"></script>
-      <script src="https://files.bpcontent.cloud/2025/03/16/14/20250316140102-PZKRH88E.js"></script>
+      <script src="https://files.bpcontent.cloud/2025/03/17/15/20250317155730-TC5UD9JS.js"></script>
     </div>
   );
 };
